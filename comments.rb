400cbce3fbbf88f3f4fb4b2762505e8a5acd3c24
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 07:47:30 -0300

    feat(basics): comments

---
+++ comments.rb
@@ -0,0 +1,23 @@
+# puts "Standard Input and Output"
+
+# puts "Enter your name:"
+
+# gets
+# gets()
+# variable1 = gets
+
+# "Vin\xA1cius\n"
+# .chomp remove the line break
+
+=begin
+
+Your name is "Vin\u0000cius\n"
+puts "Your name is " + variable1.inspect
+
+Your name is Vincius
+puts "Your name is " + variable1.chomp
+
+puts "Enter your age:"
+variable2 = gets.chomp
+puts "Your age is " + variable2 # test comment
+=end

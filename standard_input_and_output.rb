802fb1964064d31908dfff6105f22607ae33524f
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 07:43:50 -0300

    feat(basics): standard input and output

---
+++ standard_input_and_output.rb
@@ -0,0 +1,20 @@
+puts "Standard Input and Output"
+
+puts "Enter your name:"
+
+# gets
+# gets()
+variable1 = gets
+
+# "Vin\xA1cius\n"
+# .chomp remove the line break
+
+# Your name is "Vin\u0000cius\n"
+puts "Your name is " + variable1.inspect
+
+# Your name is Vincius
+puts "Your name is " + variable1.chomp
+
+puts "Enter your age:"
+variable2 = gets.chomp
+puts "Your age is " + variable2

f80e748cba3315f5939a35c217e4afafde42f8f0
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 08:29:27 -0300

    feat(basics): strings and variables interpolations

---
+++ src/basics/strings_and_variables_interpolations.rb
@@ -0,0 +1,12 @@
+puts "Hello, World!"
+puts 'Are u ok?'
+
+# using the '+' generates a new object by changing the memory address
+variable1 = "Vinícius"
+puts "Hello! " + variable1
+
+# variables interpolations
+puts "Are u ok? #{variable1}"
+puts 'Are u ok? #{variable1}'
+
+puts "The sum of 1 + 2 is #{1 + 2}"

6c959c2c87f5b3fcfba17eb86a3bcff03458f45f
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 20:39:48 -0300

    feat(basics): arrays

---
+++ src/basics/arrays.rb
@@ -0,0 +1,17 @@
+array = []
+
+array.push(67)
+array.push(90)
+array.push(45)
+
+puts array # show all elements
+
+puts array[1] # show specific element
+
+array[2] = 34
+
+puts array
+
+array.delete(90)
+
+puts array

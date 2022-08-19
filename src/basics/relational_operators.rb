f52534f89cfc7b49f0ba4d90f4c6db980beaa3e6
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 12:05:21 -0300

    feat(basics): relational operators

---
+++ src/basics/relational_operators.rb
@@ -0,0 +1,15 @@
+puts 1 > 2
+puts 1 < 2
+puts 1 >= 3
+puts 1 >= 1
+puts 1 <= 4
+puts 5 <= 6
+puts 2 == 2
+puts 2 == 0
+puts 2 != 3
+puts 2 != 2
+
+# <=> Spaceship
+puts 1 <=> 2
+puts 2 <=> 2
+puts 2 <=> 1

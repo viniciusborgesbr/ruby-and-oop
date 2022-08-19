4b15aa86ceefe7218a9814556c36f7a8a072a352
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 12:23:05 -0300

    feat(basics): assignment operators

---
+++ src/basics/assignment_operators.rb
@@ -0,0 +1,23 @@
+variable1 = 2
+variable1 += 4
+puts variable1
+
+variable2 = 2
+variable2 -= 4
+puts variable2
+
+variable3 = 2
+variable3 *= 4
+puts variable3
+
+variable4 = 2
+variable4 /= 4
+puts variable4
+
+variable5 = 2
+variable5 %= 4
+puts variable5
+
+variable6 = 2
+variable6 **= 4
+puts variable6

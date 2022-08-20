4a39cef5f40893f58556eacf2914be9d52886cd1
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-19 22:48:20 -0300

    feat(basics): symbols

---
+++ src/basics/symbols.rb
@@ -0,0 +1,17 @@
+puts "Vinícius".object_id
+puts "Vinícius".object_id
+puts "Vinícius".object_id
+
+puts "Vinícius".class
+
+puts :abc
+
+puts :abc.object_id
+puts :abc.object_id
+puts :abc.object_id
+
+# {key: "value"}
+# {:a => "123aaa"}
+puts {a:"123aaa"}
+
+puts "asd".to_sym

a6e994b9498514fd6f8f43f499d90ef6d2491ba0
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-21 10:16:32 -0300

    feat(oop): const. and const. class

---
+++ src/oop/const_and_const_class.rb
@@ -0,0 +1,15 @@
+# the difference between variable and constant,
+# is that the constant you declare with capital letters.
+# PI = 3.14159
+
+# Const. Class
+
+class TestClass
+    PI = 3.14
+    NAME = "Vinícius Borges"
+    WAGE = 3141.59
+end
+
+puts TestClass::PI
+puts TestClass::NAME
+puts TestClass::WAGE

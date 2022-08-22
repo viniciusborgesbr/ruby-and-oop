b3a70e334ee0d3bdbb63ce57a1b184a1d3ee66c7
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-22 08:38:54 -0300

    feat(extras): gems

---
+++ src/extras/gems.rb
@@ -0,0 +1,13 @@
+# gem install cpf_utils
+require 'cpf_utils'
+
+puts CpfUtils.cpf_formatado
+
+# gem install lerolero_generator
+require 'lerolero_generator'
+
+puts LeroleroGenerator.sentence
+puts LeroleroGenerator.paragraph
+
+# gem install <gem>
+# gem unistall <gem>

532f7852d3b4db78827555ed814cf96363cc2cfc
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-22 08:49:49 -0300

    feat(extras): ARGV

--- src/extras/files/file.rb
+++ src/extras/files/file.rb
@@ -2,12 +2,33 @@
 #     fil.puts "Vinícius Borges - Ruby and OOP"
 # end
 
-File.open('test.txt', 'r') do  |fil|
-    # while line = fil.gets
-    #     puts line
-    # end
+# File.open('test.txt', 'r') do  |fil|
+#     # while line = fil.gets
+#     #     puts line
+#     # end
 
-    while line = fil.gets do
-        puts line
+#     while line = fil.gets do
+#         puts line
+#     end
+# end
+
+puts ARGV # Argument Vector
+
+puts ARGV.size
+puts ARGV[0]
+puts ARGV[1]
+puts ARGV[2]
+
+# ruby file.rb test.txt Vinícius Borges
+
+puts "------------"
+
+if ARGV.size > 0
+    File.open(ARGV[0], 'r') do  |fil|
+        while line = fil.gets do
+            puts line
+        end
     end
+else
+    puts "File not found."
 end

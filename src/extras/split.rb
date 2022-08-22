47f6f7aa261c533f58b34cd46d8129abddcfb847
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-22 07:56:18 -0300

    feat(extras): split

---
+++ src/extras/split.rb
@@ -0,0 +1,14 @@
+variable1 = "Vinícius Borges"
+
+puts variable1.split
+
+puts variable1.class
+puts variable1.split.class
+
+puts variable1.split('')
+puts variable1.split("")
+
+variable2 = "Vinícius/Borges"
+
+puts variable2.split('/')
+puts variable2.split("/")

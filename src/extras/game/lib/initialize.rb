69bba51277c609371bfbfee58c3aa2d6a60711a8
Vinícius Borges <viniciusborgesbr@outlook.com>
2022-08-22 23:08:44 -0300

    feat(extras): gems for terminal

--- src/extras/game/lib/initialize.rb
+++ src/extras/game/lib/initialize.rb
@@ -1,8 +1,22 @@
+require 'tty-cursor'
+require 'tty-spinner'
+require 'tty-table'
+
 class Initialize
     def self.initializing
         system('cls')
 
+        spinner = TTY::Spinner.new
+        spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
+        spinner.auto_spin
+        sleep(5)
+        spinner.stop('Done!')
+
+        cursor = TTY::Cursor
+        print cursor.move_to(5, 2)
+
         print "Starting..."
+        
 
         4.times do |i|
             sleep 1
@@ -10,5 +24,8 @@ class Initialize
         end
 
         puts "."
+
+        table = TTY::Table.new ['header1', 'header2'], [['a1', 'a2'], ['b1', 'b2']]
+        table.render(:ascii)
     end
 end
